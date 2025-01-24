272f28bfb447ff0bea9144d4fc3df101efdcb4e6
RAKSHITH R <108052912+RakshithR07@users.noreply.github.com>
2025-01-25 00:25:08 +0530

    feign client implemented

--- src/main/java/com/sr/inventory_service/controller/InventoryController.java
+++ src/main/java/com/sr/inventory_service/controller/InventoryController.java
@@ -16,7 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sr.inventory_service.entity.Inventory;
+import com.sr.inventory_service.pojo.InventoryPojo;
+import com.sr.inventory_service.pojo.StorePojo;
 import com.sr.inventory_service.service.InventoryService;
+import com.sr.inventory_service.service.StoreClient;
 
 @RestController
 @RequestMapping("/api")
@@ -25,14 +28,17 @@ public class InventoryController {
 	@Autowired
 	InventoryService inventoryService;
 	
+	@Autowired
+	StoreClient storeClient;
+	
 	@GetMapping("/inventorys")
 	public ResponseEntity<List<Inventory>> getAllInventorys(){
 		return new ResponseEntity<List<Inventory>>(inventoryService.getAllInventorys(),HttpStatus.OK);
 	}
-	@GetMapping("/inventorys/{inid}")
-	public ResponseEntity<Optional<Inventory>> getAInventory(@PathVariable int inid){
-		return new ResponseEntity<Optional<Inventory>>(inventoryService.getAInventory(inid),HttpStatus.OK);
-	}
+//	@GetMapping("/inventorys/{inid}")
+//	public ResponseEntity<Optional<Inventory>> getAInventory(@PathVariable int inid){
+//		return new ResponseEntity<Optional<Inventory>>(inventoryService.getAInventory(inid),HttpStatus.OK);
+//	}
 	@PostMapping("/inventorys")
 	public ResponseEntity<Inventory> addInventory(@RequestBody Inventory newInventory){
 		return new ResponseEntity<Inventory>(inventoryService.addInventory(newInventory),HttpStatus.OK);
@@ -42,10 +48,36 @@ public class InventoryController {
 		return new ResponseEntity<Inventory>(inventoryService.updateInventory(editInventory),HttpStatus.OK);
 	}
 	
-	@DeleteMapping("inventorys/{inid}")
+	@DeleteMapping("/inventorys/{inid}")
 	public ResponseEntity<Void> deleteInventory(@PathVariable int inid){
 		inventoryService.deleteInventory(inid);
 		return new ResponseEntity(HttpStatus.OK);
+	}
+	
+	@GetMapping("/inventorys/{inid}")
+	public ResponseEntity<InventoryPojo> getAInventory(@PathVariable int inid) {
+	    Optional<Inventory> inOptional = inventoryService.getAInventory(inid);
+	    
+	    if (inOptional.isPresent()) {
+	        Inventory inventory = inOptional.get();  // Unwrap the Optional
+	        
+	        InventoryPojo inPojo = new InventoryPojo();
+	        
+	        // Now use Feign client to fetch department by departmentId
+	        StorePojo store = storeClient.getStoreById(inventory.getInStoreId());
+	        
+	        
+	        // Set the department to the employee
+	        inPojo.setInventoryId(inventory.getInventoryId());
+	        inPojo.setProductQuantity(inventory.getProductQuantity());
+	        inPojo.setInStoreId(inventory.getInStoreId());
+	        inPojo.setInProductId(inventory.getInProductId());
+	        inPojo.setStorePojo(store);
+	        
+	        return new ResponseEntity<>(inPojo, HttpStatus.OK);
+	    } else {
+	        return new ResponseEntity<>(HttpStatus.NOT_FOUND);  // Employee not found
+	    }
 	}
 
 
---
+++ src/main/java/com/sr/inventory_service/service/StoreClient.java
@@ -0,0 +1,16 @@
+package com.sr.inventory_service.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.sr.inventory_service.pojo.StorePojo;
+
+@FeignClient(name = "stores-service", url = "http://localhost:7001/api")  // URL of the Department Service
+public interface StoreClient {
+
+    // Endpoint to fetch department details by department ID
+    @GetMapping("/stores/{storeId}")
+    StorePojo getStoreById(@PathVariable int storeId);
+}
+
